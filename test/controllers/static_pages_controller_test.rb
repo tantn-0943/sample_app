e0815c14e420d2ae929ec8228057bc137e7ce6ac
tantn-0943 <tran.ngoc.tan@sun-asterisk.com>
2019-08-27 13:54:12 +0700

    finished Chapter_3: Mostly satic pages

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,38 @@
+require "test_helper"
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get root" do
+    get "/"
+    assert_response :success
+  end
+
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select "title",
+      "#{I18n.t 'static_pages.home.title'} | #{@base_title}"
+  end
+
+  test "should get help" do
+    get static_pages_help_url
+    assert_response :success
+    assert_select "title",
+      "#{I18n.t 'static_pages.help.title'} | #{@base_title}"
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title",
+      "#{I18n.t 'static_pages.about.title'} | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+  end
+end
