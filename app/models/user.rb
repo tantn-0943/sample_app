ad71b48ed8838b552921512c98a9d9292216d0ae
tantn-0943 <tran.ngoc.tan@sun-asterisk.com>
2019-09-03 14:45:00 +0700

    Chaper9: Advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save{email.downcase!}
   validates :name, presence: true, length: {maximum: Settings.length.name}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -16,5 +17,23 @@ class User < ApplicationRecord
     else
       BCrypt::Password.create(string, cost: BCrypt::Engine.cost)
     end
+  end
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false unless remember_digest
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute :remember_digest, nil
   end
 end
